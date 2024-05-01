cd7d90d2a5607dbd4e24122148f9b1c8895550f3
vale <valentino.losito@dunelm.com>
2024-05-01 16:33:09 +0100

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,20 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      black: "rgb(0,0,0)",
+      red: "rgb(187,0,0)",
+      green: "rgb(0,187,0)",
+      yellow: "rgb(255,255,85)",
+      blue: "rgb(0,0,187)",
+      magenta: "rgb(187,0,187)",
+      cyan: "rgb(0,187,187)",
+      white: "rgb(191,191,191)",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["var(--font-mesloLGS)"],
       },
     },
   },
